47110ad369cf88dad1fe5cfb235c3bb53e6c6b6e
Hubert Strękowski <hstrekowski05@gmail.com>
2026-02-06 22:44:23 +0100

    feat: Added better solution O(n)

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -2,14 +2,35 @@
 
 class Program
 {
+    // public int[] TwoSum(int[] nums, int target)
+    // {
+    //     for (int i = 0; i < nums.Length - 1; i++)
+    //     {
+    //         for (int j = i + 1; j < nums.Length; j++)
+    //         {
+    //             if (nums[i] + nums[j] == target)
+    //                 return [i, j];
+    //         }
+    //     }
+
+    //     return [];
+    // }
     public int[] TwoSum(int[] nums, int target)
     {
-        for (int i = 0; i < nums.Length - 1; i++)
+        Dictionary<int, int> seenNumbers = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
         {
-            for (int j = i + 1; j < nums.Length; j++)
+            int neededValue = target - nums[i];
+
+            if (seenNumbers.ContainsKey(neededValue))
             {
-                if (nums[i] + nums[j] == target)
-                    return [i, j];
+                return [seenNumbers[neededValue], i];
+            }
+
+            if (!seenNumbers.ContainsKey(nums[i]))
+            {
+                seenNumbers[nums[i]] = i;
             }
         }
 
