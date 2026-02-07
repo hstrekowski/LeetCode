c6f7e6a1578666d4d7b37cb2f039099b3f0f4f30
Hubert Strękowski <hstrekowski05@gmail.com>
2026-02-07 10:29:17 +0100

    feat: MySqrt(x) done - above average solution

---
+++ Sqrt/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Sqrt;
+
+class Program
+{
+    public static int MySqrt(int x)
+    {
+        int sqrt = 0;
+        for (long i = 0; i * i <= x; i++)
+        {
+            sqrt = (int)i;
+        }
+        return sqrt;
+    }
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MySqrt(2147395600));
+    }
+}
