38f3404367f49fc793d55ba867ed3b9e015dc283
Hubert Strękowski <hstrekowski05@gmail.com>
2026-02-07 10:31:14 +0100

    feat: Little optimalization to MySqrt(x)

--- Sqrt/Program.cs
+++ Sqrt/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     public static int MySqrt(int x)
     {
+        if (x < 2) return x;
         int sqrt = 0;
         for (long i = 0; i * i <= x; i++)
         {
@@ -11,7 +12,7 @@ class Program
         }
         return sqrt;
     }
-    }
+
     static void Main(string[] args)
     {
         Console.WriteLine(MySqrt(2147395600));
