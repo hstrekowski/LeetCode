4a4f1ed712e9b14c5d142c8ae7ec33a8bda6fc9c
Hubert Strękowski <hstrekowski05@gmail.com>
2026-02-07 11:23:13 +0100

    feat: ExcelSheetColumnTable done - 0ms runtime

---
+++ ExcelSheetColumnTable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace ExcelSheetColumnTable;
+
+class Program
+{
+    public static string ConvertToTitle(int columnNumber)
+    {
+        if (columnNumber-- <= 0) return string.Empty;
+
+        string actual = ((char)((columnNumber % 26) + 65)).ToString();
+
+        return ConvertToTitle(columnNumber / 26) + actual;
+    }
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine($"{27} - {ConvertToTitle(27)}");
+    }
+}
