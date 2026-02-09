36a2c43ee73c6574bc60f9e0d5b653abab792410
Hubert Strękowski <hstrekowski05@gmail.com>
2026-02-09 12:42:56 +0100

    feat: PlusOne done - above average solution

---
+++ PlusOne/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PlusOne;
+
+class Program
+{
+    public int[] PlusOne(int[] digits)
+    {
+        digits[digits.Length - 1] = (digits[digits.Length - 1] + 1) % 10;
+
+        for (int i = digits.Length - 1; i > 0; i--)
+        {
+            if (digits[i] == 0)
+            {
+                digits[i - 1] = (digits[i - 1] + 1) % 10;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return digits.All(x => x == 0) ? [1, .. new int[digits.Length]] : digits;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
