0646ae7b9b9b8efdd0e692f8f4bcc049bce7ae21
Hubert Strękowski <hstrekowski05@gmail.com>
2026-02-10 17:54:31 +0100

    feat: MergeTwoSortedLists done - above average solution

---
+++ MergeTwoSortedLists/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MergeTwoSortedLists;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Program
+{
+    public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val <= list2.val)
+            {
+                curr.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        curr.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
