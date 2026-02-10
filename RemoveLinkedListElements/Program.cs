c52a388038a6dea3af42737589cc1c044cc940e3
Hubert Strękowski <hstrekowski05@gmail.com>
2026-02-10 18:21:01 +0100

    feat: RemoveLinkedListElements done - above average solution

---
+++ RemoveLinkedListElements/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace RemoveLinkedListElements;
+
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+class Program
+{
+    public static ListNode RemoveElements(ListNode head, int val)
+    {
+        if (head == null) return null;
+
+        ListNode curr = head;
+
+        while (curr != null && curr.next != null)
+        {
+            if (curr.next.val == val)
+                curr.next = curr.next.next;
+            else
+            {
+                curr = curr.next;
+            }
+        }
+
+        return head.val == val ? head.next : head;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
