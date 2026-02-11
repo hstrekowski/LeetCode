7b1028ce913005751001e14b52b8a4da4026d675
Hubert Strękowski <hstrekowski05@gmail.com>
2026-02-11 02:52:52 +0100

    feat: ConvertBinNumInALinkedListToInteger done

---
+++ ConvertBinNumberInALinkedListToInteger/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Text;
+
+namespace ConvertBinNumberInALinkedListToInteger;
+
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+class Program
+{
+    public static int GetDecimalValue(ListNode head)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        while (head != null)
+        {
+            sb.Append(head.val);
+            head = head.next;
+        }
+
+        return Convert.ToInt32(sb.ToString(), 2);
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
