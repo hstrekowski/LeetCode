ff5769b920f28e559be5ccb130fe7553452161b6
Hubert Strękowski <hstrekowski05@gmail.com>
2026-02-18 01:51:59 +0100

    feat: 876 Done - average solution

---
+++ MiddleOfTheLinkedList/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MiddleOfTheLinkedList;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Program
+{
+    public static ListNode MiddleNode(ListNode head)
+    {
+        ListNode result = head;
+        ListNode toEnd = head;
+
+        while (toEnd != null && toEnd.next != null)
+        {
+            result = result.next;
+            toEnd = toEnd.next.next;
+        }
+
+        return result;
+    }
+    static void Main()
+    {
+
+    }
+}
