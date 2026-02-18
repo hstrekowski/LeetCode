8c5b2e35b5511d2b88a38eb805b70489281a3e0f
Hubert Strękowski <hstrekowski05@gmail.com>
2026-02-18 02:13:01 +0100

    141 Done - average solution

---
+++ LinkedListCycle/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace LinkedListCycle;
+
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+class Program
+{
+
+    public static bool HasCycle(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) return true;
+        }
+
+        return false;
+    }
+    static void Main() { }
+}
